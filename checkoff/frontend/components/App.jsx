bec51fe689d095a031c85678220521206f83aff6
RawBData <lunarmonky@yahoo.com>
2019-09-10 14:32:46 -0700

    Working on banner

--- checkoff/frontend/components/App.jsx
+++ checkoff/frontend/components/App.jsx
@@ -3,12 +3,13 @@ import { Route } from 'react-router-dom';
 import LoginFormContainer from './login_signup_form/login_form_container';
 import SignupFormContainer from './login_signup_form/signup_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import BannerContainer from './banner/banner_container';
 
 
 const App = () => (
   <div>
     <div>
-      <h1 className="logo-letters">CHECKOFF</h1>
+      <BannerContainer className="banner"/>
       <div className="logo">
       </div>
     </div>
--- checkoff/frontend/components/banner/banner.jsx
+++ checkoff/frontend/components/banner/banner.jsx
@@ -1,1 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Banner extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    const currentUser = this.props.currentUser;
+    const logout = this.props.logout;
+    // const likedAllPageToggle = this.props.location.pathname === '/chirps'? 
+    //   {title:"Your Liked Chirps!",to:"/likedchirps"} 
+    //   : 
+    //   {title:"All The Chirps!",to:"/chirps"} ;
+
+    const display = currentUser ? (
+      <div>
+        <div>
+          {/* <Link className="btn" to={likedAllPageToggle.to}>{likedAllPageToggle.title}</Link> */}
+          <h3>Welcome {currentUser.username}!</h3>
+        </div>
+        <button onClick={logout}>Logout</button>
+      </div>
+    ) : (
+      <div>
+        <Link className="btn" to="/signup">Sign Up</Link>
+        <Link className="btn" to="/login">Log In</Link>
+      </div>
+    );
+
+    return (
+      <header className="banner">
+        <h1 className="logo">CHECKOFF</h1>
+        <figure>
+          <img src="" alt="" />
+        </figure>
+        <div>
+          {display}
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Banner;
