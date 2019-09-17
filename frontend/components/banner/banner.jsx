bf48eae85f2e717f1eb51e1de4d05df222f2b758
RawBData <lunarmonky@yahoo.com>
2019-09-17 00:16:06 -0700

    possibly mistakes

--- frontend/components/banner/banner.jsx
+++ frontend/components/banner/banner.jsx
@@ -24,11 +24,11 @@ class Banner extends React.Component {
       </div>
     ) : (
       <div className="banner-nav">
-        <Link className="button tablet-change-nav-button mobile-hide" to="/">Tour</Link>
-        <Link className="button tablet-change-nav-button mobile-hide" to="/">What's New</Link>
-        <Link className="button tablet-change-nav-button mobile-hide" to="/">Upgrade</Link>
-        <Link className="button tablet-change-nav-button mobile-hide" to="/">Help</Link>
-        <Link className="button tablet-change-nav-button last-button" to="/login">Log in</Link>
+        <Link className="button" to="/">Tour</Link>
+        <Link className="button" to="/">What's New</Link>
+        <Link className="button" to="/">Upgrade</Link>
+        <Link className="button" to="/">Help</Link>
+        <Link className="button last-button" to="/login">Log in</Link>
         <div className="sign-up-demo">
           <Link className="button signup-button" to="/signup">Sign up for free</Link>
           <Link onClick={()=>this.props.demoLogin({username:"Demo",password:"123456"})} className="button demo" to="/">Demo</Link>
