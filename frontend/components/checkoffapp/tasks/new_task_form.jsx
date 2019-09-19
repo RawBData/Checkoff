36920c79387b79bfd9adc27c990c3089a7082d14
RawBData <lunarmonky@yahoo.com>
2019-09-19 15:53:28 -0700

    more tasks work

--- frontend/components/checkoffapp/tasks/new_task_form.jsx
+++ frontend/components/checkoffapp/tasks/new_task_form.jsx
@@ -12,26 +12,49 @@ import React from "react";
 class NewTaskForm extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            newTask: "",
+        }
     }
 
     componentDidMount(){
         
+    }
+
+    handleSubmit(){
+
     }
     
     render(){
     
         const display = (
             <div className="tasks">
-                {/* <ul>
-                    {this.props.tasks.map(task => (<li key={task.id}><TasksIndexItem task={task}/></li>))}
-                </ul> */}
-                <h1>New Task Form Goes Here</h1>
+                <div className="task-input-container">
+                    <input className="task-input" type="text" placeholder="Add a task..." value={this.state.newTask} onChange={this.fieldChange}/>
+                </div>
+
+                <div className="add-task-actions">
+                    <div className="add-task-optional-tags-container">
+                        <i className="fa fa-calendar-check-o"></i>
+                        <i className="fa fa-calendar-plus-o"></i>
+                        <i className="fa fa-exclamation"></i>
+                        <i className="fa fa-list-alt"></i>
+                        <i className="fa fa-refresh"></i>
+                        <i className="fa fa-map-marker"></i>
+                        <i className="fa fa-clock-o"></i>
+                        <i className="fa fa-user"></i> 
+                    </div>
+
+                    <div className="add-task-button-container">
+                        <button onClick={()=>this.handleSubmit}>Add Task</button>
+                    </div>
+                </div>
             </div>
         );
     
         return (
     
-        <main>
+        <main className="task-form-component-container">
             {display}
         </main>
         );
--- frontend/components/checkoffapp/tasks/tasks_index.jsx
+++ frontend/components/checkoffapp/tasks/tasks_index.jsx
@@ -6,13 +6,16 @@ import React from "react";
 // import BannerContainer from '../banner/banner_container';
 // import Tasks from './tasks/tasks';
 import TasksIndexItem from './tasks_index_item';
-import NewTaskForm from './tasks_index_item';
+import NewTaskForm from './new_task_form';
 
 
 
 class TasksIndex extends React.Component {
     constructor(props){
         super(props)
+        this.state={
+            tasksSelected: false,
+        }
     }
 
     componentDidMount(){
@@ -21,16 +24,40 @@ class TasksIndex extends React.Component {
     
     render(){
 
+        const actionsRow2Display = this.state.tasksSelected?
+        ("task-specific-actions")
+        :
+        ("hide-task-specific-actions");
+
             
-        const tasks_index_buttons = (<div></div>);
     
         const display = (
+            
             <div className="tasks_index">
-                {/* <hr/> */}
+                <div className="tasks-index-actions">
+                    <div className="actions-row-1">
+                        <div className="action-cog-container" onClick={logout}>
+                            <i className="fa fa-cog tasks-cog"></i>
+                            <i className="fa fa-caret-down tasks-cog-carrot"></i>
+                        </div>
+                        <h5 className="action-link-complete">complete</h5>
+                        <h5 className="action-link-complete">incomplete</h5>
+                        <i className="fa fa-print print-icon"></i>
+                    </div>
+                    <div className="actions-row-2">
 
-                <ul>
-                    {this.props.tasks.map(task => (<li key={task.id}><TasksIndexItem task={task}/></li>))}
-                </ul>
+                    </div>
+                </div>
+
+                <div className="tasks-index-new-task-form-container">
+                    <NewTaskForm />
+                </div>
+
+                <div className="task-index-tasks">
+                    <ul>
+                        {this.props.tasks.map(task => (<li key={task.id}><TasksIndexItem task={task}/></li>))}
+                    </ul>
+                </div>
                 
             </div>
         );
@@ -39,6 +66,7 @@ class TasksIndex extends React.Component {
     
         <main className="task-index-container">
             {display}
+            
         </main>
         );
     }
